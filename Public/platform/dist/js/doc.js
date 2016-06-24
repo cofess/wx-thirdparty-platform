b07ed24971749ca94438c6520f75416b01686cca
Devwork <547306864@qq.com>
2016-06-24 15:16:30 +0800

    单页面模版编辑页面更新；添加smartwidgets插件；

--- Public/platform/dist/js/doc.js
+++ Public/platform/dist/js/doc.js
@@ -1,13 +1,28 @@
-/**
- * smoke config
- */
-$(function () {
+(function ($) {
+	//pace preload
+	$(document).ajaxStart(function() { Pace.restart(); }); 
+	
+	//smoke config
 	$('#fullscreen').smkFullscreen();
 	$(".smkPanel").smkPanel({hide: 'remove'});
 	$(".smkPanel-min").smkPanel({hide: 'remove,full'});
 	$(".smkPanel-full").smkPanel();
-});
-/**
- * pace
- */
-$(document).ajaxStart(function() { Pace.restart(); }); 
+
+	//Make the dashboard widgets sortable Using jquery UI
+  	// $(".connectedSortable").sortable({
+   //  	placeholder: "sort-highlight",
+   //  	connectWith: ".connectedSortable",
+   //  	handle: ".sortable-handle",
+   //  	forcePlaceholderSize: true,
+   //  	zIndex: 999999
+  	// });
+  	// $(".connectedSortable .sortable-content").css("cursor", "move");
+
+  	//jQuery UI sortable for the todo list
+  	// $(".todo-list").sortable({
+   //  	placeholder: "sort-highlight",
+   //  	handle: ".handle",
+   //  	forcePlaceholderSize: true,
+   //  	zIndex: 999999
+  	// });
+})(jQuery);
